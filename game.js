03f2489dc555e30fe4e0badd18078dbd0c7f58bc
AndreiBalasca <46415669+BobliHH@users.noreply.github.com>
2024-04-15 15:23:29 +0300

    finished code from first step (readme.md)

--- game.js
+++ game.js
@@ -1,4 +1,10 @@
+let gamePattern = [];
+let buttonColours = ["red", "blue", "green", "yellow"];
+
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   console.log(randomNumber);
 }
+randomChosenColour = buttonColours[randomNumber];
+console.log(randomChosenColour);
+gamePattern.push(randomChosenColour);

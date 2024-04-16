83665d5049e37461ac1506838205dbaef70b9ea5
AndreiBalasca <46415669+BobliHH@users.noreply.github.com>
2024-04-16 14:03:31 +0300

    Update game.js

--- game.js
+++ game.js
@@ -3,8 +3,28 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
-  console.log(randomNumber);
+  return randomNumber;
 }
 randomChosenColour = buttonColours[randomNumber];
 console.log(randomChosenColour);
 gamePattern.push(randomChosenColour);
+
+let buttonId = "#" + randomChosenColour;
+$(buttonId).click(function () {
+  $(this).animate(
+    {
+      backgroundColor: "#FF0000", // flash color
+      color: "#FFFFFF", // text color
+    },
+    100,
+    function () {
+      $(this).delay(100).animate(
+        {
+          backgroundColor: "#FFFFFF", // original background color
+          color: "#000000", // original text color
+        },
+        100
+      );
+    }
+  );
+});
